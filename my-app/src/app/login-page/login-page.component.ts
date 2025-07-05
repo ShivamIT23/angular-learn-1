2d97784f8a84d90dbab8cb5397bdae9b4d524250
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2025-07-06 02:26:31 +0530

    Form validation

--- my-app/src/app/login-page/login-page.component.ts
+++ my-app/src/app/login-page/login-page.component.ts
@@ -1,11 +1,48 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule,NgIf],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+  // name=new FormControl('test');
+  // password=new FormControl('123');
 
+  // displayValue(){
+  //   console.log(this.name.value,this.password.value)
+  // }
+
+  formData = new FormGroup({
+    name: new FormControl('',[Validators.required,Validators.maxLength(20)]),
+    email: new FormControl('',[Validators.required,Validators.email]),
+    password: new FormControl('',Validators.minLength(5))
+  })
+
+  onSubmit(){
+    console.log(this.formData.value)
+  }
+
+  get name(){
+    return this.formData.get('name')
+  }
+
+  get email(){
+    return this.formData.get('email')
+  }
+
+  get password(){
+    return this.formData.get('password')
+  }
+
+  setValues(){
+    this.formData.setValue({
+      name:"shivam",
+      email:"shivam@gmail.com",
+      password:"shivam 2311"
+    })
+  }
 }
--- my-app/src/app/user/user.component.ts
+++ my-app/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  constructor(private route:ActivatedRoute){}
+  ngOnInit(){
+    this.route.params.subscribe((params)=>{
+      console.log(params)
+    })
+  }
 }
